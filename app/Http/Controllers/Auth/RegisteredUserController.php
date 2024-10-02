dfd216aeaefb42c33a72e1700c10b97d51b59f1f
abukafa <hikam.abukafa@gmail.com>
2024-10-03 06:47:02 +0700

    feat: setup spatie

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -21,7 +21,7 @@ class RegisteredUserController extends Controller
      */
     public function create(): Response
     {
-        return Inertia::render('Auth/Register');
+        return Inertia::render("Auth/Register");
     }
 
     /**
@@ -32,16 +32,18 @@ class RegisteredUserController extends Controller
     public function store(Request $request): RedirectResponse
     {
         $request->validate([
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:'.User::class,
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            "name" => "required|string|max:255",
+            "email" => "required|string|email|max:255|unique:" . User::class,
+            "password" => ["required", "confirmed", Rules\Password::defaults()],
         ]);
 
         $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
+            "name" => $request->name,
+            "email" => $request->email,
+            "password" => Hash::make($request->password),
         ]);
+
+        $user->assignRole("user");
 
         event(new Registered($user));
 
